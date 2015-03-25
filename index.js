0ad8c04b5e79091369e3ba04631766fa8943c117
Perevedko <alex45510@ya.ru>
2015-03-25 03:03:13 +0300

    updates
    message length limit and codestyle improvement

---
+++ index.js
@@ -0,0 +1,35 @@
+var message_max_length = 200;
+
+var msgBox = $('#msgBox');
+var box = $('#msg');
+
+$(function(){
+    $('#btnSend').click(send);
+    msgBox.keyup(function(event){
+        if(event.which == 13) {
+            send();
+            return false;
+        }
+    });
+});
+ws = new WebSocket("ws://localhost:8001");
+//ws = new WebSocket("ws://162.208.11.89:8001");
+
+ws.onopen = function(){};
+ws.onclose = function(){
+    alert("Connection closed...");
+};
+ws.onmessage = function(evt) {
+    box.append("<p>" + evt.data + "</p>");
+    box.scrollTop(box[0].scrollHeight);
+};
+
+function send() {
+    var message = msgBox.val();
+    if (message.length > message_max_length) {
+        alert('Message is too long');
+        return;
+    }
+    ws.send(msgBox.val());
+    msgBox.val('');
+}
